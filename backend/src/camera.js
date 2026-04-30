4195d784055dcadd5f56af18174d53c779855ffd
Zan e1 <zbeidas@icloud.com>
2026-04-30 19:50:46 -0400

    some changes to the way clients are loogged in run

--- backend/src/camera.js
+++ backend/src/camera.js
@@ -19,8 +19,57 @@ const getParams = () => {
 } 
 
 // Execute the ffmpeg command with the parameters
-const ffmpegProcess = spawn('ffmpeg', getParams())
+let ffmpegProcess = null;
+let ffmpegOn = false;
+let clients = new Set();
 
+const startFFmpeg = () => {
+  if (ffmpegOn) return;
+  console.log('======== Starting ffmpeg ========');
+  ffmpegProcess = spawn('ffmpeg', getParams());
+
+  // Pipe the stdout of the ffmpeg process directly to the client because it's already in the correct format
+  ffmpegProcess.stdout.on('data', (chunk) => {
+    for (const res of clients) {
+      try {
+        // Write the chunk to the client
+        res.write(chunk);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })
+
+  let frameCount = 0;
+  const frameLogFrequency = 100;
+  // Logging for errors and individual frame data
+  ffmpegProcess.stderr.on('data', (chunk) => {
+    const data = chunk.toString();
+
+    // Log all errors
+    if (data.includes('error')) {
+      console.error(data);
+    }
+
+    // Log frames by the frequency
+    if (data.includes('frame')) {
+      frameCount++;
+      if (frameCount % frameLogFrequency === 0) {
+        console.log(`${data}\n`);
+      }
+    }
+  });
+  ffmpegOn = true;
+}
+
+const stopFFmpeg = () => {
+  if (!ffmpegOn) return;
+  console.log('======== Stopping ffmpeg ========');
+  cleanup();
+  ffmpegOn = false;
+}
+
+// Kills the ffmpeg process
 export const cleanup = () => {
     if (!ffmpegProcess.killed) {
         ffmpegProcess.kill('SIGTERM');
@@ -30,20 +79,44 @@ export const cleanup = () => {
 // Route to get a snapshot from the camera
 router.get('/stream', (req, res) => {
     try {
+        if (clients.size === 0) {
+          startFFmpeg();
+        }
+
+        clients.add(res);
+        console.log('Added client, total clients:', clients.size)
+
         // Defines how we will send the data to the client
         res.writeHead(200, {
           "Content-Type": "multipart/x-mixed-replace; boundary=ffmpeg", 
         });
-        
-        // Pipe the stdout of the ffmpeg process directly to the client because it's already in the correct format
-        ffmpegProcess.stdout.pipe(res);
 
-        // Logging for errors and individual frame data
-        ffmpegProcess.stderr.on('data', (chunk) => {
-          console.error(chunk.toString());
+        req.on('close', () => {
+          clients.delete(res);
+          console.log('Removed client, total clients:', clients.size)
+          if (clients.size === 0) {
+            stopFFmpeg();
+          }
+        });
+
+        req.on('aborted', () => {
+          clients.delete(res);
+          console.log('Removed client by aborted, total clients:', clients.size)
+          if (clients.size === 0) {
+            stopFFmpeg();
+          }
+        });
+
+        req.on('error', (_) => {
+          clients.delete(res);
+          console.log('Removed client by error, total clients:', clients.size)
+          if (clients.size === 0) {
+            stopFFmpeg();
+          }
         });
     } catch (error) {
         console.error(error);
+
         res.status(500).json({message: 'Error getting stream'});
     }
 });
