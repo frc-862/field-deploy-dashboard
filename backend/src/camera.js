a7100d7585a82194b6084ea29221fb17eefb172e
Zan e1 <zbeidas@icloud.com>
2026-04-30 15:18:30 -0400

    Working code-- streams with a bitrate of around 3500kbit/s

--- backend/src/camera.js
+++ backend/src/camera.js
@@ -6,43 +6,45 @@ const router = express.Router();
 // Function to return parameters for ffmpeg command
 const getParams = () => {
   return [
-    '-f', 'avfoundation',
-    '-framerate', '30',
-    '-i', '0:none',
-    '-frames:v', '10',
-    '-f', 'uyvy422',
-    '-q:v', '5',
-    'pipe:1'
+    '-f', 'avfoundation', // Use avfoundation for macOS
+    '-framerate', '30', // Frame rate to use from the input device
+    '-pixel_format', 'uyvy422', // Pixel format to use from the input device
+    '-video_size', '1280x720', // Video size to use from the input device
+    '-i', '0:none', // Input device to use
+    '-vf', 'fps=15', // Frame rate to use for the output
+    '-q:v', '15', // Quality (how much compression to use) of the output
+    '-f', 'mpjpeg', // Output format
+    'pipe:1' // Output to pipe 1 (stdout)
   ]
 } 
 
-// Route to get a snapshot from the camera
-router.get('/snapshot', (req, res) => {
-    try {
-        // Execute the ffmpeg command with the parameters
-        const process = spawn('ffmpeg', getParams())
+// Execute the ffmpeg command with the parameters
+const ffmpegProcess = spawn('ffmpeg', getParams())
 
+export const cleanup = () => {
+    if (!ffmpegProcess.killed) {
+        ffmpegProcess.kill('SIGTERM');
+    }
+}
+
+// Route to get a snapshot from the camera
+router.get('/stream', (req, res) => {
+    try {
         // Defines how we will send the data to the client
         res.writeHead(200, {
-          "Content-Type": "multipart/x-mixed-replace; boundary=frame", 
-          "Connection": "keep-alive", 
-          "Cache-Control": "no-cache"
+          "Content-Type": "multipart/x-mixed-replace; boundary=ffmpeg", 
         });
         
-        process.stdout.on('data', (chunk) => {
-          res.write("--frame\r\n");
-          res.write("Content-Type: image/jpeg\r\n\r\n");
+        // Pipe the stdout of the ffmpeg process directly to the client because it's already in the correct format
+        ffmpegProcess.stdout.pipe(res);
 
-          res.write(chunk);
-          res.write("\r\n");
-        });
-
-        process.stderr.on('data', (chunk) => {
+        // Logging for errors and individual frame data
+        ffmpegProcess.stderr.on('data', (chunk) => {
           console.error(chunk.toString());
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({message: 'Error getting snapshot'});
+        res.status(500).json({message: 'Error getting stream'});
     }
 });
 
