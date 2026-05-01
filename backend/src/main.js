728d83e90af01e5770064f3b404caa210af64bec
Zan e1 <zbeidas@icloud.com>
2026-05-01 13:34:30 -0400

    Added cloning endpoint-- writes to repos directory

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cameraRouter from './camera.js';
+import githubRouter from './github.js';
 import { cleanup } from './camera.js';
 
 const app = express();
 
 // Include the camera router with the main express app
 app.use('/camera', cameraRouter);
+app.use('/github', githubRouter);
 
 // When the server is closed, cleanup the ffmpeg process
 app.on('close', cleanup);
