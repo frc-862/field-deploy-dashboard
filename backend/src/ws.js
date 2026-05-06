7522a5a1c8951587b93149e1eebaafcb2d056c9a
Zan e1 <zbeidas@icloud.com>
2026-05-06 19:06:11 -0400

    Good enough

--- backend/src/ws.js
+++ backend/src/ws.js
@@ -12,22 +12,6 @@ wss.on('connection', (ws) => {
     clients.add(ws);
     console.log('Client connected to WebSocket ✅');
 
-    ws.on('message', (message) => {
-        // let counter = 0;
-
-        // console.log('Received message from client:', message.toString());
-
-        // for (const client of clients) {
-        //     if (client !== ws) {
-        //         client.send(message.toString());
-        //         counter++;
-        //     }
-        // }
-        // console.log(`Message sent to ${counter} clients`);
-
-        console.warn('Client sent a message, ignored it ❌');
-    });
-
     ws.on('close', () => {
         clients.delete(ws);
         console.log('Client disconnected from WebSocket ❌');
